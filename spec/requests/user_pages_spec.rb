7f17a6f20707b8f20f4c180955e902c7c3f772b6
czatup <czat.up@gmail.com>
2012-08-29 02:17:26 +0200

    Refactoring

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -40,10 +40,10 @@ describe "User pages" do
 
     describe "with valid information" do
       before do
-        fill_in "Name", with: "Example User"
-        fill_in "Email", with: "user@example.com"
+        fill_in "Name",     with: "Example User"
+        fill_in "Email",    with: "user@example.com"
         fill_in "Password", with: "foobar"
-        fill_in "Confirmation", with: "foobar"
+        fill_in "Confirm Password", with: "foobar"
       end
 
       it "should create a user" do
@@ -62,7 +62,39 @@ describe "User pages" do
     end
   end
 
-  # describe "home page" do
-  #   tutaj test na brak sign in!
-  # end
+  describe "edit" do
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit edit_user_path(user) }
+
+    describe "page" do
+      it { should have_selector('h1',    text: "Update your profile") }
+      it { should have_selector('title', text: "Edit user") }
+      it { should have_link('change', href: 'http://gravatar.com/emails') }
+    end
+
+    describe "with invalid information" do
+      before { click_button "Save changes" }
+
+      it { should have_content('error') }
+    end
+
+    describe "with valid information" do
+      let(:new_name)  { "New Name" }
+      let(:new_email) { "new@example.com"}
+
+      before do
+        fill_in "Name",               with: new_name
+        fill_in "Email",              with: new_email
+        fill_in "Password",           with: user.password
+        fill_in "Confirm Password",   with: user.password
+        click_button "Save changes"
+      end
+
+      it { should have_selector('title', text: new_name) }
+      it { should have_selector('div.alert.alert-success') }
+      it { should have_link('Sign out', href: signout_path) }
+      specify { user.reload.name.should   == new_name }
+      specify { user.reload.email.should  == new_email }
+    end
+  end
 end
